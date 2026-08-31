c49eda12c26b414767391500135b2d441152de0e
bab2sky <107404985+bab2sky@users.noreply.github.com>
2026-09-01 00:11:52 +0900

    refactor: export crypto analyst factories

--- tradingagents/agents/__init__.py
+++ tradingagents/agents/__init__.py
@@ -1,10 +1,10 @@
-from .analysts.fundamentals_analyst import create_fundamentals_analyst
 from .analysts.market_analyst import create_market_analyst
 from .analysts.news_analyst import create_news_analyst
-from .analysts.sentiment_analyst import (
-    create_sentiment_analyst,
-    create_social_media_analyst,  # deprecated alias kept for back-compat
-)
+from .analysts.sentiment_analyst import create_sentiment_analyst
+from .analysts.onchain_analyst import create_onchain_analyst
+from .analysts.derivatives_analyst import create_derivatives_analyst
+from .analysts.order_flow_analyst import create_order_flow_analyst
+from .analysts.macro_analyst import create_macro_analyst
 from .managers.portfolio_manager import create_portfolio_manager
 from .managers.research_manager import create_research_manager
 from .researchers.bear_researcher import create_bear_researcher
@@ -17,21 +17,9 @@ from .utils.agent_states import AgentState, InvestDebateState, RiskDebateState
 from .utils.agent_utils import create_msg_delete
 
 __all__ = [
-    "AgentState",
-    "create_msg_delete",
-    "InvestDebateState",
-    "RiskDebateState",
-    "create_bear_researcher",
-    "create_bull_researcher",
-    "create_research_manager",
-    "create_fundamentals_analyst",
-    "create_market_analyst",
-    "create_neutral_debator",
-    "create_news_analyst",
-    "create_aggressive_debator",
-    "create_portfolio_manager",
-    "create_conservative_debator",
-    "create_sentiment_analyst",
-    "create_social_media_analyst",  # deprecated; will be removed in a future version
-    "create_trader",
+    "AgentState", "InvestDebateState", "RiskDebateState", "create_msg_delete",
+    "create_market_analyst", "create_sentiment_analyst", "create_news_analyst",
+    "create_onchain_analyst", "create_derivatives_analyst", "create_order_flow_analyst", "create_macro_analyst",
+    "create_bear_researcher", "create_bull_researcher", "create_research_manager", "create_trader",
+    "create_aggressive_debator", "create_neutral_debator", "create_conservative_debator", "create_portfolio_manager",
 ]
